ac18a1afffa98d7c769d434985e7abdf5271a934
shahria jaman khan <41187226+shahria7k@users.noreply.github.com>
2021-08-13 20:34:29 +0600

    bug resolved

--- 01-expanding-cards/app.js
+++ 01-expanding-cards/app.js
@@ -1,9 +1,10 @@
 const panels = document.querySelectorAll('.panel');
 
 panels.forEach(element => {
-    element.addEventListener('click', (e) => {
+    element.addEventListener('click', (event) => {
+        //e.terget.parentNode.childNodes.forEach(item=>{item.classList.remove('active')})
         removeActive();
-        e.target.classList.add('active');
+        event.target.classList.add('active');
     });
 });
 function removeActive() {
