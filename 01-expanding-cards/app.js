aff9018faf0c8244dc5545d5b0385a7aadc971e5
shahria jaman khan <41187226+shahria7k@users.noreply.github.com>
2021-08-13 20:35:50 +0600

    bug fixed : forEach() is not a function

--- 01-expanding-cards/app.js
+++ 01-expanding-cards/app.js
@@ -2,7 +2,11 @@ const panels = document.querySelectorAll('.panel');
 
 panels.forEach(element => {
     element.addEventListener('click', (event) => {
-        //e.terget.parentNode.childNodes.forEach(item=>{item.classList.remove('active')})
+        /**
+         * !  e.terget.parentNode.childNodes.forEach(item=>{item.classList.remove('active')})
+         * ? Why it says forEach() is not a function?
+         * ? Should I run for loop instead?
+         */
         removeActive();
         event.target.classList.add('active');
     });
